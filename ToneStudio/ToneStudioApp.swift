11809fe28b6c8d3ea61f0f344d56c14545e4937a
Upen Krishnan <upenkrishnan@example.com>
2026-02-20 17:06:50 +0530

    Initial Commit

---
+++ ToneStudio/ToneStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneStudioApp.swift
+//  ToneStudio
+//
+//  Created by Upen on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ToneStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
